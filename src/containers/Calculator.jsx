f1118c29756773a72c214d7fd8c15dbbfa0f24b2
DaubaKao <ab6060118@gmail.com>
2021-03-18 00:31:39 +0800

    update

--- src/containers/Calculator.jsx
+++ src/containers/Calculator.jsx
@@ -1,28 +1,40 @@
-const buttons = [
-  { text: 'AC', color: 'gray', type: 'fun' },
-  { text: '+/-', color: 'gray', type: 'fun' },
-  { text: '%', color: 'gray', type: 'fun' },
-  { text: '/', color: 'blue', type: 'op' },
-  { text: 'x', color: 'blue', type: 'op' },
-  { text: '-', color: 'blue', type: 'op' },
-  { text: '+', color: 'blue', type: 'op' },
-  { text: '=', color: 'blue', type: 'op' },
-  { text: '7', color: 'black', type: 'digit' },
-  { text: '8', color: 'black', type: 'digit' },
-  { text: '9', color: 'black', type: 'digit' },
-  { text: '4', color: 'black', type: 'digit' },
-  { text: '5', color: 'black', type: 'digit' },
-  { text: '6', color: 'black', type: 'digit' },
-  { text: '1', color: 'black', type: 'digit' },
-  { text: '2', color: 'black', type: 'digit' },
-  { text: '3', color: 'black', type: 'digit' },
-  {
-    text: '0', color: 'black', type: 'digit', width: 2,
-  },
-  { text: '.', color: 'black', type: 'digit' },
+const buttonGroups = [
+  [
+    { text: 'AC', color: 'gray', type: 'fun' },
+    { text: '+/-', color: 'gray', type: 'fun' },
+    { text: '%', color: 'gray', type: 'fun' },
+    { text: '/', color: 'blue', type: 'op' },
+  ],
+  [
+    { text: '7', color: 'black', type: 'digit' },
+    { text: '8', color: 'black', type: 'digit' },
+    { text: '9', color: 'black', type: 'digit' },
+    { text: 'x', color: 'blue', type: 'op' },
+  ],
+  [
+    { text: '4', color: 'black', type: 'digit' },
+    { text: '5', color: 'black', type: 'digit' },
+    { text: '6', color: 'black', type: 'digit' },
+    { text: '-', color: 'blue', type: 'op' },
+  ],
+  [
+    { text: '1', color: 'black', type: 'digit' },
+    { text: '2', color: 'black', type: 'digit' },
+    { text: '3', color: 'black', type: 'digit' },
+    { text: '+', color: 'blue', type: 'op' },
+  ],
+  [
+    {
+      text: '0', color: 'black', type: 'digit', width: 2,
+    },
+    { text: '.', color: 'black', type: 'digit' },
+    { text: '=', color: 'blue', type: 'op' },
+  ],
 ];
 
-const Button = ({ text, color, handler }) => (
+const Button = ({
+  text, color, handler, width,
+}) => (
   <button type="button" className={`${color} calculator-button`} onClick={handler}>{text}</button>
 );
 
@@ -30,32 +42,17 @@ const Calculator = () => (
   <div className="calculator">
     <div className="calculator-screen">screen</div>
     <div className="calculator-body">
-      <div className="calculator-col">
-        {
-          buttons.filter(({ type }) => ['fun', 'digit'].includes(type))
-            .map(({ text, color, width }) => (
-              <div
-                className="calculator-col-cell"
-                key={text}
-                style={{
-                  ...(width && { flexBasis: `${width * 33}%` }),
-                }}
-              >
-                <Button color={color} text={text} />
-              </div>
-            ))
-        }
-      </div>
-      <div className="calculator-col">
-        {
-          buttons.filter(({ type }) => type === 'op')
-            .map(({ text, color }) => (
-              <div className="calculator-col-cell" key={text}>
-                <Button color={color} text={text} />
-              </div>
-            ))
-        }
-      </div>
+      {
+        buttonGroups.map((buttons) => (
+          <div className="calculator-row">
+            {
+              buttons.map(({ text, color, width }) => (
+                <Button color={color} text={text} width={width} />
+              ))
+            }
+          </div>
+        ))
+      }
     </div>
   </div>
 );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        18: '4.5rem',
+      },
+      height: {
+        18: '4.5rem',
+      },
+    },
   },
   variants: {
     extend: {},
